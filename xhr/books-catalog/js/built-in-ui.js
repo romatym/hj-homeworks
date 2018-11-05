448f749289acd2518e8597fe2d1fca8aa63f5152
Roman <roma_tym@mail.ru>
2018-11-05 21:41:07 +0300

    Merge branch 'master' of https://github.com/romatym/Netology

--- xhr/books-catalog/js/built-in-ui.js
+++ xhr/books-catalog/js/built-in-ui.js
@@ -1,11 +1,10 @@
 /* Данный JS код */
 
 // Регулируем видимость карточки
-function toggleCardVisible () {
- document.getElementById('content').classList.toggle('hidden');
- document.getElementById('card').classList.toggle('hidden');
+function toggleCardVisible() {
+    document.getElementById('content').classList.toggle('hidden');
+    document.getElementById('card').classList.toggle('hidden');
 }
-
 
 document.getElementById('close').addEventListener('click', toggleCardVisible);
 
@@ -19,10 +18,35 @@ document.getElementById('content').addEventListener('click', (event) => {
     }
 
     if (target) {
-      toggleCardVisible();
-      document.getElementById('card-title').innerHTML = target.dataset.title;
-      document.getElementById('card-author').innerHTML = target.dataset.author;
-      document.getElementById('card-info').innerHTML = target.dataset.info;
-      document.getElementById('card-price').innerHTML = target.dataset.price;
+        toggleCardVisible();
+        document.getElementById('card-title').innerHTML = target.dataset.title;
+        document.getElementById('card-author').innerHTML = target.dataset.author;
+        document.getElementById('card-info').innerHTML = target.dataset.info;
+        document.getElementById('card-price').innerHTML = target.dataset.price;
     }
 });
+
+const request = new XMLHttpRequest();
+request.addEventListener("load", onLoad);
+request.open('GET', 'https://neto-api.herokuapp.com/book/', true);
+request.send();
+function onLoad() {
+    if (this.status === 200) {
+        const response = JSON.parse(this.responseText);
+
+        var content = document.getElementById('content');
+        var innerHTML = '';
+        for (const book of response) {
+            innerHTML += [
+                '<li',
+                'data-title="' + book.title + '"',
+                'data-author="' + book.author.name + '"',
+                'data-info="' + book.info + '"',
+                'data-price="' + book.price + '">',
+                '<img src="' + book.cover.small + '">',
+                '</li>'
+            ].join(' ');
+        }
+        content.innerHTML = innerHTML;
+    }
+}
